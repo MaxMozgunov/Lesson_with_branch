76aa7babb4bb648813a7411c8a1354a77bd3736e
MaxMozgunov <136356747+MaxMozgunov@users.noreply.github.com>
2024-01-13 15:21:41 +0100

    reset all

--- Lesson/app/Students/Maksym_Mozghunov/main.py
+++ Lesson/app/Students/Maksym_Mozghunov/main.py
@@ -1,8 +1,1 @@
-if __name__ == "__main__":
-    # n1 = int(input("Enter the first number: "))
-    # n2 = int(input("Enter the degree: "))
-
-
-    def exponentiation(self,n1, n2):
-        return n1 ** n2
-
+print("Hello, World")
