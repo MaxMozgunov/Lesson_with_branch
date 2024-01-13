c5456c02464c6018f84b9abe162b8de51b51c13c
MaxMozgunov <136356747+MaxMozgunov@users.noreply.github.com>
2024-01-13 15:14:59 +0100

    call def exponentiation

--- Lesson/app/Students/Maksym_Mozghunov/main.py
+++ Lesson/app/Students/Maksym_Mozghunov/main.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
-    n1 = int(input("Enter the first number: "))
-    n2 = int(input("Enter the degree: "))
+    # n1 = int(input("Enter the first number: "))
+    # n2 = int(input("Enter the degree: "))
 
 
-    def exponentiation(n1, n2):
+    def exponentiation(self,n1, n2):
         return n1 ** n2
 
--- Lesson/app/views.py
+++ Lesson/app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+import Students.Maksym_Mozghunov.main as func
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
     return render(request, "index.html")
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the degree: "))
+
+print(func.exponentiation(n1, n2))
