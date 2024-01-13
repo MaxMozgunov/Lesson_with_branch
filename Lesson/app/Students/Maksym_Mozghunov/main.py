ec93682b01cb8fbc0b1c39f811cf362a85a7dac3
MaxMozgunov <136356747+MaxMozgunov@users.noreply.github.com>
2024-01-13 15:02:17 +0100

    exponentiation

--- Lesson/app/Students/Maksym_Mozghunov/main.py
+++ Lesson/app/Students/Maksym_Mozghunov/main.py
@@ -1,5 +1,8 @@
-n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the degree: "))
-def exponentiation(n1, n2):
-    return n1**n2
+if __name__ == "__main__":
+    n1 = int(input("Enter the first number: "))
+    n2 = int(input("Enter the degree: "))
+
+
+    def exponentiation(n1, n2):
+        return n1 ** n2
 
